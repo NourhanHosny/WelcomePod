35f4ea8e2ca86b3647b221203648b6665703efda
Nourhan Hosny <nourhan.work95@gmail.com>
2018-08-26 14:13:57 +0200

    Initial Commit

---
+++ WelcomePod/WelcomePod.h
@@ -0,0 +1,19 @@
+//
+//  WelcomePod.h
+//  WelcomePod
+//
+//  Created by Nourhan Hosny on 8/26/18.
+//  Copyright © 2018 Nourhan Hosny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WelcomePod.
+FOUNDATION_EXPORT double WelcomePodVersionNumber;
+
+//! Project version string for WelcomePod.
+FOUNDATION_EXPORT const unsigned char WelcomePodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WelcomePod/PublicHeader.h>
+
+
